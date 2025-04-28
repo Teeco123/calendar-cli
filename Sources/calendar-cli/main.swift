bd0984650be670753a1edb84e62660f5ff2e6ae8
Kacper Gajko <kacper.gajko1@icloud.com>
2025-04-28 09:40:07 +0200

    searching for calendar by number

--- Sources/calendar-cli/main.swift
+++ Sources/calendar-cli/main.swift
@@ -2,11 +2,16 @@ import ArgumentParser
 import EventKit
 import Foundation
 
+struct MyCalendar {
+    var id: Int
+    var calendar: EKCalendar
+}
+
 @main
-struct CLI: ParsableCommand {
+struct CalendarCli: ParsableCommand {
     static let configuration = CommandConfiguration(
         abstract: "A CLI with multiple subcommands.",
-        subcommands: [List.self]
+        subcommands: [List.self, Create.self]
     )
 }
 
@@ -21,6 +26,17 @@ struct List: ParsableCommand {
     func run() throws {
         let eventManager = EventManager()
         try eventManager.listTodayEvents(today: today)
+    }
+}
+
+struct Create: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Creates new event."
+    )
+
+    func run() throws {
+        let eventManager = EventManager()
+        try eventManager.createNewEvent()
     }
 }
 
@@ -63,6 +79,55 @@ class EventManager {
         displayEvents(events)
     }
 
+    func createNewEvent() throws {
+        guard try requestAccess() else { return }
+        let newEvent = EKEvent(eventStore: eventStore)
+        let calendar: EKCalendar
+
+        name()
+
+        func name() {
+            print("Creating new event (Step 1 of ?)")
+            print("Enter event name: ")
+            guard let input = readLine(), !input.isEmpty else {
+                print("Name cannot be empty. Please try again.")
+                name()
+                return
+            }
+            newEvent.title = input
+            chooseCalendar()
+        }
+
+        func chooseCalendar() {
+            print("Creating new event (Step 2 of ?)")
+            print("Choose your calendar number: ")
+            let calendars = listCalendars()
+            for calendar in calendars {
+                print(calendar.id, calendar.calendar.title)
+            }
+            guard let input = readLine(), !input.isEmpty else {
+                print("You need to choose your calendar. Please try again.")
+                chooseCalendar()
+                return
+            }
+            if let foundIndex = calendars.first(where: { $0.id == Int(input) }) {
+                print("Found calendar: \(foundIndex)")
+            }
+        }
+
+        guard let defaultCalendar = eventStore.defaultCalendarForNewEvents else {
+            print("⚠️ No default calendar available")
+            return
+        }
+        calendar = defaultCalendar
+
+        newEvent.startDate = Date()
+        newEvent.endDate = Date()
+        newEvent.calendar = calendar
+
+        try eventStore.save(newEvent, span: .thisEvent, commit: true)
+    }
+
     private func displayEvents(_ events: [EKEvent]) {
         print("\nEvents")
         print("------------------------------")
@@ -90,5 +155,17 @@ class EventManager {
             print("------------------------------")
         }
     }
-}
 
+    private func listCalendars() -> [MyCalendar] {
+        let calendars = eventStore.calendars(for: .event)
+        var arr: [MyCalendar] = []
+
+        var i = 1
+        for calendar in calendars {
+            let cal = MyCalendar(id: i, calendar: calendar)
+            arr.append(cal)
+            i += 1
+        }
+        return arr
+    }
+}
